6ad1146a8ad6cf8b999b87c33a8f6898e3e0774b
Daniel Pedro <dp4144036@gmail.com>
2025-10-07 17:48:11 +0100

    created respository-team

---
+++ src/domain/repositories/team-repository.ts
@@ -0,0 +1,13 @@
+import type { PaginationParams } from "../../utils/pagination-params";
+import type { Team } from "../Entites/Team";
+
+export interface ITeamRepository {
+    create(team: Team): Promise<Team>
+    findByTeamName(TeamName: string): Promise<Team | undefined>
+    findByEmail(Email: string): Promise<Team | undefined>
+    findById(Id: string): Promise<Team | undefined>
+    fetchMany( param:PaginationParams):Promise<Team[]>
+    findAll(): Promise<Team[]>
+    save(team: Team): Promise<void>
+    delete(team: Team): Promise<void>
+}
